2c3c6133e1302b0708f20a712eceb995d099cbb6
FichBickit <cabinchroma@gmail.com>
2025-11-25 18:54:51 +1100

    Starting JavaScript

---
+++ script.js
@@ -0,0 +1,20 @@
+const submit = document.querySelector(".submit");
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
+const nameRegex = /^(?!.*[-'].*[-'])(?=.*[A-Za-z])[A-Za-z'-]+$/;
+
+valid = false;
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (nameRegex.test(firstName.value) && nameRegex.test(lastName.value)) {
+        valid = true;
+        console.log(valid);
+    } else {
+        valid = false;
+        console.log(valid);
+    }
+
+})
+
+
