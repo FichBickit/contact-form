249311ffa7d9938ed5fe737d7342f4e2fa3d8b11
FichBickit <cabinchroma@gmail.com>
2025-11-26 18:59:43 +1100

    Finished for day

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const submit = document.querySelector(".submit");
 const firstName = document.querySelector("#firstName");
 const lastName = document.querySelector("#lastName");
+const firstNameEmpty = document.querySelector(".firstNameEmpty");
+const firstNameFormatError = document.querySelector(".firstNameFormatError");
+const lastNameEmpty = document.querySelector(".lastNameEmpty");
+const lastNameFormatError = document.querySelector(".lastNameFormatError");
+const error = document.querySelector(".error");
+
 const nameRegex = /^(?!.*[-'].*[-'])(?=.*[A-Za-z])[A-Za-z'-]+$/;
+
+firstNameEmpty.classList.remove("error");
 
 valid = false;
 
@@ -9,13 +17,13 @@ submit.addEventListener("click", (e) => {
     e.preventDefault();
     if (nameRegex.test(firstName.value)) {
         valid = true;
-        
         console.log(valid);
     } else {
         valid = false;
+        firstNameEmpty.classList.add("error");
         console.log(valid);
     }
-
+    
 })
 
 
